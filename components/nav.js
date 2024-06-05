bbf896c845778d705a29af6f3d5692db398f01af
Braydon Olinger <braydonolingermktg@gmail.com>
2024-06-05 15:05:14 -0500

    nav item change

--- components/nav.js
+++ components/nav.js
@@ -12,7 +12,7 @@ export default navItems => {
   return html`
     <nav>
       <i class="fas fa-bars"></i>
-      <ul class="">
+      <ul class="hidden--mobile">
         ${navItems.map(item => navItem(item)).join("")}
       </ul>
     </nav>
